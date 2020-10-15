b0679a276b35d978d9fcf0ce1fc234a1ce1868ef
Vanessa Stewart <vstewart1213@gmail.com>
2020-10-15 10:07:26 -0700

    Add seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Recipe.destroy_all
+Tag.destroy_all
+
+50.times do |index|
+  Recipe.create!(name: Faker::Food.dish,
+                        instructions: Faker::Food.description,
+                        ingredients: Faker::Food.ingredient,
+                        rating: rand(1..5))
+end
+
+
+p "Created #{Recipe.count} recipes"
+
+tag_list = ["Breakfast", "Lunch", "Dinner", "Dessert", "Fun", "Easy", "Intermediate", "Hard", "Soul-Crushing", "Gordon Ramsey Approved", "Dairy-free", "Vegan", "Gluten-Free", "Plant-Based Protein", "Freegan"]
+
+tag_list.each do |name|
+  Tag.create( name: name )
+end
